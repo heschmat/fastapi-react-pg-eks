d1a6fb81a7e7a6e9fb39674df56e46c4bc524904
hesch <heschmatx@gmail.com>
2025-08-05 09:59:58 +0000

    fix: add migration files to Dockerfile; prevent reload

--- backend/start.sh
+++ backend/start.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 echo "⏳ Waiting for PostgreSQL using wait-for-it.sh..."
+# db is the service name in docker-compsoe
+# and the name of the service for your pg when deploying to eks
 ./wait-for-it.sh db:5432 --timeout=30 --strict -- echo "✅ PostgreSQL is up"
 
 echo "🚀 Running Alembic migrations..."
@@ -8,4 +10,5 @@ alembic upgrade head
 # alembic upgrade head --raiseerr --config alembic.ini
 
 echo "⚙️ Starting FastAPI app..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+#exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+exec uvicorn app.main:app --host 0.0.0.0 --port 8000
